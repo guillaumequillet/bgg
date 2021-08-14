0d7cc5ac7d3690d3b05ef8f79aa0419f1f676dce
guillaumequillet <bgachats@yahoo.fr>
2021-08-14 14:03:40 +0200

    added custom mouse cursor

--- bgg/window.rb
+++ bgg/window.rb
@@ -1,5 +1,6 @@
 module BGG
   class Window < Gosu::Window
+    MOUSE_Z = 10000
     def initialize(width: 640, height: 480, fullscreen: false, caption: 'untitled')
       @width, @height, @fullscreen, @caption = width, height, fullscreen, caption
       super(@width, @height, @fullscreen)
@@ -12,13 +13,28 @@ module BGG
       @keys[:escape] = key
     end
 
-    def hide_mouse_cursor; @needs_cursor = false; end
-    def show_mouse_cursor; @needs_cursor = true; end
+    def set_mouse_icon(gosu_image)
+      @mouse_icon = gosu_image.is_a?(Gosu::Image) ? gosu_image : Gosu::Image.new(gosu_image, retro: true)
+    end
 
-    def needs_cursor?; @needs_cursor; end
+    def hide_mouse_cursor; @needs_cursor = false; end
+
+    def show_mouse_cursor; @needs_cursor; true
+    end
+
+    def needs_cursor?
+      return false if defined?(@mouse_icon)
+      @needs_cursor
+    end
 
     def button_down(id)
       self.close! if @keys.has_key?(:escape) && id == @keys[:escape]
+    end
+
+    def draw
+      if defined?(@mouse_icon) && @needs_cursor
+        @mouse_icon.draw_rot(self.mouse_x, self.mouse_y, MOUSE_Z, 0)
+      end
     end
   end
 end
--- init.rb
+++ init.rb
@@ -4,6 +4,7 @@ class Window < BGG::Window
   def initialize
     super(width: 640, height: 480, caption: 'my game')
     set_escape_key(Gosu::KB_ESCAPE)
+    set_mouse_icon('./gfx/cursor.png')
   end
 
   def update
@@ -11,12 +12,14 @@ class Window < BGG::Window
   end
 
   def draw
+    super
+
     @aabb ||= BGG::AABB.new(origin: BGG::Vector.new(x: 10, y: 10), size: BGG::Vector.new(x: 100, y: 100))
     
     mouse = BGG::AABB.new(origin: BGG::Vector.new(x: self.mouse_x, y: self.mouse_y), size: BGG::Vector.new(x: 10, y: 10))
 
     color = @aabb.collides?(mouse) ? Gosu::Color::RED : Gosu::Color::WHITE
-    @aabb.draw(color: color, style: :outline)
+    @aabb.draw(color: color, style: :solid)
     mouse.draw(color: Gosu::Color::GREEN)
   end
 end
